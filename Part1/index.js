d74a4d496cf3ff95ed8f026cb8973d1b36c61862
kobrabson <67922399+kobrabson@users.noreply.github.com>
2020-07-14 15:42:08 -0600

    I tried figuring tictac winner

--- Part1/index.js
+++ Part1/index.js
@@ -19,3 +19,45 @@ function play (clickedId){
 }
 
 
+const topLeft = `#0`
+const topCenter = `#1`
+const topRight = `#2`
+const middleLeft = `#3`
+const middleCenter = `#4`
+const middleRight = `#5`
+const bottomLeft = `#6`
+const bottomCenter = `#7`
+const bottomRight = `#8`
+
+const winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
+
+function winnings() {
+    let roundWon = false;
+    for ( let i = 0; i <= 7; i++) {
+        const winningCondition = winningConditions[i];
+        let a = gameProg[winningCondition[0]];
+        let b = gameProg[winningCondition[1]];
+        let c = gameProg[winningCondition[2]];
+        if (a === `` || b ===`` || c === ``) {
+            continue;
+        }
+        if ( a === b && b === c) {
+            roundWon = true;
+            break
+        }
+
+    }
+if (roundWon) {
+    alert(`you are the winner`)
+    console.log(winner)
+}
+}
