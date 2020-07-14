dee2c61f1cef77af724d7db084d7fbd7d32e300c
kobrabson <67922399+kobrabson@users.noreply.github.com>
2020-07-14 17:06:58 -0600

    part2 step2 done

---
+++ Part2/index.js
@@ -0,0 +1,7 @@
+console.log(`The house always wins`)
+
+const setId = document.getElementById(`idInput`)
+const setColor = document.getElementById(`colorInput`)
+
+console.log(`setId`)
+console.log(`setColor`)
