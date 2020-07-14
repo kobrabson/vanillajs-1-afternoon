02a055ab01030521a325d704b17610e4f2f01293
kobrabson <67922399+kobrabson@users.noreply.github.com>
2020-07-14 16:46:19 -0600

    found the error...

--- Part1/index.js
+++ Part1/index.js
@@ -1,63 +1,131 @@
 console.log(`working`)
 
-let gameProg = [];
+const board = [];
 
 function play (clickedId){
-    let playerClick = document.getElementById(`player`);
-    let clickedElement = document.getElementById(clickedId)
+    const playerSpan = document.getElementById(`player`);
+    const clickedElement = document.getElementById(clickedId);
 
-    if (playerClick.innerText === `X`) {
-        playerClick.innerText = `O`;
-        clickedElement.innerText = `X`
-        gameProg[clickedId] = `X`
+    if (playerSpan.innerText === `X`) {
+        playerSpan.innerText = `O`;
+        clickedElement.innerText = `X`;
+        board[clickedId] = `X`;
     } else {
-        playerClick.innerText = `X`;
-        clickedElement.innerText = `O`
-        gameProg[clickedId] = `O`
+        playerSpan.innerText = `X`;
+        clickedElement.innerText = `O`;
+        board[clickedId] = `O`;
     }
-    console.log(gameProg);
+    console.log(board);
+
+
+
+const topLeft = board[0]
+const topCenter = board[1]
+const topRight = board[2]
+const middleLeft = board[3]
+const middleCenter =board[4]
+const middleRight = board[5]
+const bottomLeft = board[6]
+const bottomCenter = board[7]
+const bottomRight = board[8]
+
+
+if ( topLeft !== undefined && topLeft === topCenter && topLeft === topRight) {
+    alert(`${topLeft} is the winner`);
+    return;
 }
 
+if ( middleLeft !== undefined && middleLeft === middleCenter && middleLeft === middleRight) {
+    alert(`${topLeft} is the winner`);
+    return;
+}
 
-const topLeft = `#0`
-const topCenter = `#1`
-const topRight = `#2`
-const middleLeft = `#3`
-const middleCenter = `#4`
-const middleRight = `#5`
-const bottomLeft = `#6`
-const bottomCenter = `#7`
-const bottomRight = `#8`
+if ( bottomLeft !== undefined && bottomLeft === bottomCenter && bottomLeft === bottomRight) {
+    alert(`${bottomLeft} is the winner`);
+    return;
+}
 
-const winningConditions = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-]
+if ( topLeft !== undefined && topLeft === middleLeft && topLeft === bottomLeft) {
+    alert(`${topLeft} is the winner`);
+    return;
+}
 
-function winnings() {
-    let roundWon = false;
-    for ( let i = 0; i <= 7; i++) {
-        const winningCondition = winningConditions[i];
-        let a = gameProg[winningCondition[0]];
-        let b = gameProg[winningCondition[1]];
-        let c = gameProg[winningCondition[2]];
-        if (a === `` || b ===`` || c === ``) {
-            continue;
-        }
-        if ( a === b && b === c) {
-            roundWon = true;
-            break
-        }
+if ( topCenter !== undefined && topCenter === middleCenter && topCenter === bottomCenter) {
+    alert(`${topCenter} is the winner`);
+    return;
+}
 
+if ( topRight !== undefined && topRight === middleRight && topRight === bottomRight) {
+    alert(`${topRight} is the winner`);
+    return;
+}
+
+if ( topLeft !== undefined && topLeft === middleCenter && topLeft === bottomRight) {
+    alert(`${topLeft} is the winner`);
+    return;
+}
+
+if ( bottomLeft !== undefined && bottomLeft === middleCenter && bottomLeft === topRight) {
+    alert(`${bottomLeft} is the winner`);
+    return;
+}
+
+let boardFull = true;
+for ( let i = 0; i <= 8; i++) {
+    if (board[i] === undefined) {
+        boardFull = false;
     }
-if (roundWon) {
-    alert(`you are the winner`)
-    console.log(winner)
 }
+if (boardFull === true) {
+    alert (`Cat's game`)
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const winningConditions = [
+//     [0, 1, 2],
+//     [3, 4, 5],
+//     [6, 7, 8],
+//     [0, 3, 6],
+//     [1, 4, 7],
+//     [2, 5, 8],
+//     [0, 4, 8],
+//     [2, 4, 6]
+// ]
+
+// function winnings() {
+//     let roundWon = false;
+//     for ( let i = 0; i <= 7; i++) {
+//         const winningCondition = winningConditions[i];
+//         let a = gameProg[winningCondition[0]];
+//         let b = gameProg[winningCondition[1]];
+//         let c = gameProg[winningCondition[2]];
+//         if (a === `` || b ===`` || c === ``) {
+//             continue;
+//         }
+//         if ( a === b && b === c) {
+//             roundWon = true;
+//             break
+//         }
+
+//     }
+// if (roundWon) {
+//     alert(`you are the winner`)
+//     console.log(winner)
+// }
+// }
