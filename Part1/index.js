f8961eef5e40c78639f92515666a957079c6d2b8
kobrabson <67922399+kobrabson@users.noreply.github.com>
2020-07-14 13:41:26 -0600

    part1step2 - 3 done

--- Part1/index.js
+++ Part1/index.js
@@ -1,1 +1,14 @@
 console.log(`working`)
+
+function play (clickedId){
+    let playerClick = document.getElementById(`player`);
+    let clickedElement = document.getElementById(clickedId)
+
+    if (playerClick.innerText === `X`) {
+        playerClick.innerText = `O`;
+        clickedElement.innerText = `X`
+    } else {
+        playerClick.innerText = `X`;
+        clickedElement.innerText = `O`
+    }
+}
